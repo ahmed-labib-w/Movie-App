9ebe936982d58adad6a96dcc4b2f52e992fe80dd
budderrar <ahmednasser82380@gmail.com>
2026-03-20 17:28:26 +0200

    okay

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "392977b46a79b61dafe4f97e52c4e86c";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query,
+    )}`,
+  );
+  const data = await response.json();
+  return data.results;
+};
